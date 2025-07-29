02275f7dc8e60898449105f6cfb012c737f1b45d
yasshh18 <yashchaurasiya964@gmail.com>
2025-07-29 23:06:10 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/", // 🟢 REPO NAME yeh hai
   plugins: [react()],
 })
